e37eac30407602754ef55386202055596bae807e
vlineros <victorlineros@gmail.com>
2019-07-05 22:46:19 -0500

    added top animation and neon effect/added more projects with backends/changed colors to green scheme

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,3 +9,47 @@ contactModalTrigger.addEventListener("click", function() {
 span.onclick = function() {
   contactModal.style.display = "none";
 };
+
+// var projectImages = document.getElementsByClassName("portfolio-pic");
+// for (let i = 0; i < projectImages.length; i++) {
+//   projectImages[i].addEventListener("mouseover", function() {
+//     var title = document.getElementById("p" + i).getAttribute("id");
+//     titleAnimation(title);
+//   });
+// }
+
+// var titleAnimation = function(target) {
+//   console.log(target);
+//   anime({
+//     targets: document.querySelector("#" + target),
+//     maxWidth: {
+//       value: "100%",
+//       duration: 2000
+//     }
+//   });
+// };
+
+anime({
+  targets: ".animate-test",
+  opacity: {
+    value: "1",
+    duration: 2000,
+    delay: 1000
+  },
+  easing: "linear"
+});
+
+anime({
+  targets: ".animate-hr",
+  height: "5px",
+  maxWidth: {
+    value: "100%",
+    duration: 2000,
+    easing: "easeInExpo"
+  }
+});
+
+// var tl = anime.timeline();
+// tl.add({
+
+// })
